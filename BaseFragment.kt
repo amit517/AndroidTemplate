a54d9fea5521e094e9e0a456b0913535ad754570
Amit Kundu <kundu.amit517@gmail.com>
2022-05-24 16:06:01 +0600

    Update BaseFragment.kt
    Import updated

--- BaseFragment.kt
+++ BaseFragment.kt
@@ -1,8 +1,5 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}#end
 
-import com.deshipay.base.view.BaseFragment
-import com.deshipay.base.viewmodel.BaseViewModel
-
 class ${NAME} : BaseFragment<${Fragment_Binding_Class}>() {
     override fun layoutId(): Int = R.layout.${layoutId}
 
